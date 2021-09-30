6e72325ef13a18d689c3210a3b9b7ebf4fb0c7ea
Theanhpham11520 <panh23368@gmail.com>
2021-09-30 17:08:38 +0700

    update api

--- src/Api/main.js
+++ src/Api/main.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const MainApi = axios.create({
-    baseURL: "http://localhost:4000",
+    baseURL: "http://demoapi.wtf/api",
     headers: {
       "Content-Type": "application/json"
     }
--- src/Api/user.js
+++ src/Api/user.js
@@ -1,22 +1,22 @@
 import { MainApi } from "./main";
 
 export const all = () => {
-    const url = `/users`;
+    const url = `/user`;
     return MainApi.get(url);
   }
   export const update = (item) => {
-    const url = `/users/${item.id}`;
+    const url = `/user/${item.id}`;
     return MainApi.put(url, item);
   };
   export const show = (id) => {
-    const url = `/users/${id}`;
+    const url = `/user/${id}`;
     return MainApi.get(url);
   };
   export const remove = (id) => {
-    const url = `/users/${id}`;
+    const url = `/user/${id}`;
     return MainApi.delete(url);
   };
-  export const create = (product) => {
-    const url = `/users`;
-    return MainApi.post(url, product);
+  export const create = (user) => {
+    const url = `/user/create`;
+    return MainApi.post(url, user);
   };
