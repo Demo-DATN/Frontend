5f70aa3c3cf47b2aa0d7643fe95d7aef22f542fe
Theanhpham11520 <panh23368@gmail.com>
2021-10-01 09:08:35 +0700

    [update] config Api Products

--- src/Api/product.js
+++ src/Api/product.js
@@ -1,22 +1,22 @@
 import { MainApi } from "./main";
 
 export const all = () => {
-    const url = `/products`;
+    const url = `/product`;
     return MainApi.get(url);
   }
   export const update = (item) => {
-    const url = `/products/${item.id}`;
+    const url = `/product/${item.id}`;
     return MainApi.put(url, item);
   };
   export const show = (id) => {
-    const url = `/products/${id}`;
+    const url = `/product/${id}`;
     return MainApi.get(url);
   };
   export const remove = (id) => {
-    const url = `/products/${id}`;
+    const url = `/product/${id}`;
     return MainApi.delete(url);
   };
   export const create = (product) => {
-    const url = `/products`;
+    const url = `/product`;
     return MainApi.post(url, product);
   };
